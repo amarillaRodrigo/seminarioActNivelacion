a69411e29dba5c1f14dce9caadbb4705c6d2f21a
Rodrigo Amarilla <83515418+amarillaRodrigo@users.noreply.github.com>
2025-03-25 13:30:59 -0300

    Actualizar index.js

--- Actividad 1 Javascript/src/routes/index.js
+++ Actividad 1 Javascript/src/routes/index.js
@@ -1,12 +1,11 @@
-// filepath: mi-sitio-web/src/routes/index.js
 const express = require('express');
 const router = express.Router();
 
-// Define una ruta principal
+
 router.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
 
-// Otras rutas pueden ser definidas aquí
+
 
 module.exports = router;
