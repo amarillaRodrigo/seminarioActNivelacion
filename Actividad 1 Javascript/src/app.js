c17b2fb93dc4e82faf177fc20e9b58b45d8d1b22
Rodrigo Amarilla <83515418+amarillaRodrigo@users.noreply.github.com>
2025-03-25 13:31:44 -0300

    Actualizar app.js

--- Actividad 1 Javascript/src/app.js
+++ Actividad 1 Javascript/src/app.js
@@ -1,19 +1,17 @@
-// filepath: c:\Users\IPF-2025\Desktop\Rodrigo Amarilla IPF\mi-sitio-web\src\app.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to serve static files
+
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
